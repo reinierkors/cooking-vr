42c7c11880b0e4063336bcabc90f709deb93147a
reinierkors <reinierkors@hotmail.com>
2018-05-22 13:54:42 +0200

    Cooking chicken

--- js/kitchen.js
+++ js/kitchen.js
@@ -1,12 +1,38 @@
 window.onload = function () {
     var knife = document.getElementById("knife");
-    var caketin = document.getElementById("caketin");
+    var rawchicken = document.getElementById("rawchicken");
+    var rawchickenleg = document.getElementById("rawchickenleg");
     var camera = document.getElementById("camera");
+    var knifeEquiped = false;
+    var rawchickenlegEquiped = false;
 
     knife.addEventListener("mouseenter", function () {
         camera.appendChild(this);
-/*         this.setAttribute('position', '0 0 0');
-        this.setAttribute('rotation', '0 0 0'); */
+        knifeEquiped = true;
+        this.setAttribute("position", "0.3 -0.5 -1");
+        this.setAttribute('rotation', '0 102 90');
+    });
+
+    rawchicken.addEventListener("mouseenter", function () {
+        if (knifeEquiped == true) {
+            this.parentNode.removeChild(this);
+            rawchickenleg.setAttribute('visible', true);
+        //   camera.appendChild(this);
+        }
+    });
+
+    rawchickenleg.addEventListener("mouseenter", function () {
+        if (knifeEquiped == true) {
+            knife.parentNode.removeChild(knife); 
+            camera.appendChild(rawchickenleg);
+            rawchickenlegEquiped = true;
+        }
+    });
+
+    oven.addEventListener("mouseenter", function() {
+        if (rawchickenlegEquiped == true) {
+            rawchickenleg.parentNode.removeChild(rawchickenleg);
+        }
     });
 };
 
